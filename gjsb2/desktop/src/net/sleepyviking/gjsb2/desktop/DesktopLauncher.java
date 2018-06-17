ebbc9f9ff4d912fa5430d086f1cf1fdb088f53ce
Nikoraito <nikoraito@gmail.com>
2018-06-17 13:50:02 -0400

    Created Project
    woo

---
+++ gjsb2/desktop/src/net/sleepyviking/gjsb2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.sleepyviking.gjsb2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.sleepyviking.gjsb2.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
