7c8ce0388ac55f6b7f9deac0500c93f1579b44ec
Derick <Drock3231@gmail.com>
2018-07-07 23:41:19 -0400

    Changed Game class to be extension of Libgdx's game class

---
+++ gjsb2/core/src/net/sleepyviking/gjsb2/GJSBGame.java
@@ -0,0 +1,14 @@
+package net.sleepyviking.gjsb2;
+
+import com.badlogic.gdx.Game;
+
+import net.sleepyviking.gjsb2.screen.ScreenGame;
+
+public class GJSBGame extends Game {
+
+	@Override
+	public void create () {
+		setScreen(new ScreenGame(this));
+
+	}
+}
